436273a9fdd16d0f3e17eef65f94cbd49ba46b69
amrmostafa251 <113519106+amrmostafa251@users.noreply.github.com>
2024-02-04 00:15:54 +0200

    Add files via upload

---
+++ lcd_config.h
@@ -0,0 +1,20 @@
+﻿/*
+ * lcd_config.h
+ *
+ * Created: 19/01/2024 08:21:21 م
+ *  Author: Almodather
+ */ 
+
+
+#ifndef LCD_CONFIG_H_
+#define LCD_CONFIG_H_
+
+#define CONTROLPORT 'a' 
+#define DATAPORT 'a'
+#define EN 0
+#define RW 1
+#define	RS 2
+#define four_bits_mode
+
+
+#endif /* LCD_CONFIG_H_ */
---
+++ timer.c
@@ -0,0 +1,47 @@
+﻿/*
+ * timer.c
+ *
+ * Created: 26/01/2024 12:09:58 ص
+ *  Author: Almodather
+ */ 
+#include "timer.h"
+void timer0_ctc_mode_with_interrupt()
+{
+	/* select CTC mode*/
+	SET_BIT(TCCR0,WGM01);
+	/* load a value in OCR0 */
+	OCR0=32;
+	/* select timer clock */
+	CLR_BIT(TCCR0,CS00);
+	CLR_BIT(TCCR0,CS01);
+	SET_BIT(TCCR0,CS02);
+	/* enable interrupt*/
+	sei();
+	SET_BIT(TIMSK,OCIE0);
+
+	
+	
+}
+void timer0_ctc_mode_outputwaveform()
+{
+	DIO_SET_PIN_DIRECTION('b',3,1);
+	/* select CTC mode*/
+	SET_BIT(TCCR0,WGM01);
+	/* load a value in OCR0 */
+	OCR0=6;
+	/* select timer clock */
+	SET_BIT(TCCR0,CS02);
+	//set OC0 mode
+	SET_BIT(TCCR0,COM00);
+}
+void timer2_OV_mode_external_clock_with_interrupt(void)
+{
+	//SET OUTPUT CLOCK SOURCE 
+	SET_BIT(ASSR,AS2);
+	//SET_PRESCALER
+	SET_BIT(TCCR2,CS22);
+	SET_BIT(TCCR2,CS20);
+	// SET THE INTERRUPT 
+	SET_BIT(TIMSK,TOIE2);
+	sei();
+}
