d9bdaeda67d09b9e51ba5462d5e8d4d756d8405e
amrmostafa251 <113519106+amrmostafa251@users.noreply.github.com>
2024-02-04 00:14:14 +0200

    Add files via upload

---
+++ USART.c
@@ -0,0 +1,41 @@
+﻿/*
+ * USART.c
+ *
+ * Created: 02/02/2024 12:47:35 ص
+ *  Author: AMR MOSTAFA
+ */ 
+#include "USART.h"
+#define F_CPU 8000000UL
+#include <util/delay.h>
+void USART_Initialize(unsigned long baud)
+{
+	//set the baud rate 
+	unsigned long ubrr;
+	ubrr= F_CPU /(10*(baud+1));
+	UBRRH=ubrr>>8;
+	UBRRL=ubrr;
+	//enable receiver and transmitter
+	SET_BIT(UCSRB,RXEN);
+	SET_BIT(UCSRB,TXEN);
+	//set the charcter size
+	UCSRC =(1>>UCSZ1)|(1>>UCSZ0)|(1>>URSEL);
+}
+void USART_send_char(unsigned char data)
+{
+	while (READ_BIT(UCSRA,UDRE)==0);
+	UDR =data;
+}
+unsigned char USART_receive(void )
+{
+	while (READ_BIT(UCSRA,RXC)==0);
+	return UDR;
+}
+void USART_send_string(unsigned char *data)
+{
+	while (*data!=0)
+	{
+		USART_send_char(*data);
+		data++;
+		_delay_ms(100);
+	}
+}
---
+++ USART.h
@@ -0,0 +1,22 @@
+﻿/*
+ * USART.h
+ *
+ * Created: 02/02/2024 12:47:49 ص
+ *  Author: AMR MOSTAFA
+ */ 
+
+
+#ifndef USART_H_
+#define USART_H_
+#include <avr/io.h>
+#include "std_macros.h"
+
+void USART_Initialize(unsigned long baud);
+void USART_send_char(unsigned char data);
+unsigned char USART_receive(void );
+void USART_send_string(unsigned char *data);
+
+
+
+
+#endif /* USART_H_ */
